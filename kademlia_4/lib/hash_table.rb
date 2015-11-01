b30b72434c86cb3ef8fc73b72d6da1ce37a9e5d4
W-M <w-m@gmx.us>
2015-11-02 00:05:27 +0100

    hash table implementation. Has functionality to refresh values.

--- kademlia_4/lib/hash_table.rb
+++ kademlia_4/lib/hash_table.rb
@@ -4,8 +4,11 @@ require 'fileutils'
 #Could maybe improved with Bloom filter.
 class HashTable
 
-	def initialize(root_path)
+	attr_reader :root_path, :tRefresh
+
+	def initialize(root_path, tRefresh=1)
 		@root_path = Pathname.new(root_path) #Key/values are stored in this folder. Relative path to current directory.
+		@tRefresh = tRefresh #amount of seconds until values should be refreshed.
 	end
 
 	def fetch(key)
@@ -28,7 +31,7 @@ class HashTable
 		end
 		file_path = key_to_path(used_key)
 
-		FileUtils.mkpath(file_path.parent)
+		FileUtils.mkpath(file_path.dirname)
 		File.open(file_path, 'wb') do |f|
 			f.write(value)
 		end
@@ -44,9 +47,39 @@ class HashTable
 		return @root_path.join(folder_name, file_name)
 	end
 
+	def path_to_key(path)
+		last_part = path.dirname.to_s
+		first_part = path.basename.to_s
+		return first_part + last_part
+	end
+
 
 	def exists?(key)
 		File.exists? key_to_path(key)
+	end
+
+	#Should be called with a block.
+	#Runs the block with a {key: key, value: value} hash
+	#for each k/v that has been stored longer than @tRefresh seconds ago (and thus should be refreshed).
+	#If block has a truth-ey outcome, the value's timestamp is then updated, which means that it will take @tRefresh seconds again before re-appearing in this list.
+	#
+	#The function returns 'true' if any values have been refreshed, false otherwise.
+	def values_to_refresh
+		newer_than = Time.now - @tRefresh
+		change_happened = false
+		Dir[@root_path.join("*/*")].each do |e|
+			unless File.directory? e #Skip directories
+				puts "File: #{e}"
+				if File.mtime(e) < newer_than
+					result = yield({key: path_to_key(Pathname.new(e)), value: File.binread(e)})
+					if result
+						FileUtils.touch(e)
+						change_happened = true
+					end
+				end
+			end
+		end
+		return change_happened
 	end
 
 end
